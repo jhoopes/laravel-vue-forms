349ff698d93467aed42f2a1e0bd6173cf9daa202
John Hoopes <john.z.hoopes@gmail.com>
2018-07-23 11:18:22 -0500

    Adding ability to load multiple form configurations instead of a singular configuration

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 Route::group(['namespace' => 'Forms', 'prefix'], function() {
 
+    Route::get('/configuration', 'FormConfiguration@index');
     Route::get('/configuration/{formConfigId}', 'FormConfiguration@show');
 
     Route::post('/submit', 'FormSubmit@store');
     Route::patch('/submit', 'FormSubmit@update');
 
 });
+
--- src/Http/Controllers/Api/Forms/FormConfiguration.php
+++ src/Http/Controllers/Api/Forms/FormConfiguration.php
@@ -8,10 +8,35 @@ use jhoopes\LaravelVueForms\Http\Controllers\Controller;
 class FormConfiguration extends Controller
 {
 
+    /**
+     * Index function to retrieve multipl form configurations based on their IDs or names
+     *
+     * @param Request $request
+     */
+    public function index(Request $request)
+    {
+        $this->validate($request, [
+            'formConfigIds'     => 'nullable|array',
+            'formConfigIds.*'   => 'integer',
+            'formConfigNames'   => 'nullable|array',
+            'formConfigNames.*' => 'string',
+        ]);
+
+        $query = \jhoopes\LaravelVueForms\Models\FormConfiguration::query();
+        $query->with('fields');
+
+        if(!is_null($request->get('formConfigIds'))) {
+            $query->whereIn('id', $request->get('formConfigIds'));
+        }else if(!is_null($request->get('formConfigNames'))) {
+            $query->whereIn('name', $request->get('formConfigNames'));
+        }
+
+        return $query->get();
+    }
+
 
     public function show(Request $request, $formConfigId)
     {
         return \jhoopes\LaravelVueForms\Models\FormConfiguration::with('fields')->findOrFail($formConfigId);
     }
-
 }
